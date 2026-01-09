2ef3146f67ac4cab89503cf8ec9648569cbedf04
doanvnminh <doanvnminh160@gmail.com>
2026-01-09 18:08:07 +0700

    Update frontend to use cloud API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://my-first-login-api.onrender.com';
 
 function getInfo() {
     const username = document.getElementById('usernameInput').value;
